193215d7c51a693bec40132ce1fff7671af8f724
Chayel Sandy <cgsandy29@gmail.com>
2026-01-28 19:01:55 -0400

    Initial commit

---
+++ resources/views/pages/prototype/users/chef-list/index.blade.php
@@ -0,0 +1,93 @@
+<x-mylayouts.layout-prototype>
+
+
+
+    <style>
+        .chef-card {
+            min-height: 100px;
+        }
+    </style>
+
+    <div class="card1">
+        <div class="chef-card card mb-3 p-3">
+            <div class="row d-flex align-items-center">
+                <div class="col-md-3 text-center">
+                    <img style="width:150px; height:150px" class="rounded-circle" src="https://placehold.co/600x600"
+                        alt="Image here">
+                </div>
+                <div class="col-md-9">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores sapiente quas at similique
+                        sunt! Officia quo suscipit nostrum aut alias?</p>
+
+                    <a class="btn btn-primary" href="portfolio.html">View Profile</a>
+                    <a class="btn btn-primary" href="portfolio.html">View Portfolio</a>
+                </div>
+            </div>
+        </div>
+        <div class="chef-card card mb-3 p-3">
+            <div class="row d-flex align-items-center">
+                <div class="col-md-3 text-center">
+                    <img style="width:150px; height:150px" class="rounded-circle"
+                        src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D"
+                        alt="Image here">
+                </div>
+                <div class="col-md-9">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores sapiente quas at similique
+                        sunt! Officia quo suscipit nostrum aut alias?</p>
+
+                    <a class="btn btn-primary" href="portfolio.html">View Profile</a>
+                    <a class="btn btn-primary" href="portfolio.html">View Portfolio</a>
+                </div>
+            </div>
+        </div>
+        <div class="chef-card card mb-3 p-3">
+            <div class="row d-flex align-items-center">
+                <div class="col-md-3 text-center">
+                    <img style="width:150px; height:150px" class="rounded-circle" src="https://placehold.co/600x600"
+                        alt="Image here">
+                </div>
+                <div class="col-md-9">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores sapiente quas at similique
+                        sunt! Officia quo suscipit nostrum aut alias?</p>
+
+                    <a class="btn btn-primary" href="portfolio.html">View Profile</a>
+                    <a class="btn btn-primary" href="portfolio.html">View Portfolio</a>
+                </div>
+            </div>
+        </div>
+        <div class="chef-card card mb-3 p-3">
+            <div class="row d-flex align-items-center">
+                <div class="col-md-3 text-center">
+                    <img style="width:150px; height:150px" class="rounded-circle" src="https://placehold.co/600x600"
+                        alt="Image here">
+                </div>
+                <div class="col-md-9">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores sapiente quas at similique
+                        sunt! Officia quo suscipit nostrum aut alias?</p>
+
+                    <a class="btn btn-primary" href="portfolio.html">View Profile</a>
+                    <a class="btn btn-primary" href="portfolio.html">View Portfolio</a>
+                </div>
+            </div>
+        </div>
+        <div class="chef-card card mb-3 p-3">
+            <div class="row d-flex align-items-center">
+                <div class="col-md-3 text-center">
+                    <img style="width:150px; height:150px" class="rounded-circle" src="https://placehold.co/600x600"
+                        alt="Image here">
+                </div>
+                <div class="col-md-9">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores sapiente quas at similique
+                        sunt! Officia quo suscipit nostrum aut alias?</p>
+
+                    <a class="btn btn-primary" href="portfolio.html">View Profile</a>
+                    <a class="btn btn-primary" href="portfolio.html">View Portfolio</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</x-mylayouts.layout-prototype>
