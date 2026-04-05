2336b524a0e63b30b483fcd5f07961898a39ab9d
chvy-aust <cgsandy29@gmail.com>
2026-04-05 19:06:03 -0400

    New Commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,172 @@
 import './bootstrap';
+import AOS from 'aos';
+import Swiper from 'swiper';
+
+(function() {
+  "use strict";
+
+  /**
+   * Apply .scrolled class to the body as the page is scrolled down
+   */
+  function toggleScrolled() {
+    const selectBody = document.querySelector('body');
+    const selectHeader = document.querySelector('#header');
+    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+  }
+
+  document.addEventListener('scroll', toggleScrolled);
+  window.addEventListener('load', toggleScrolled);
+
+  /**
+   * Mobile nav toggle
+   */
+  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavToggleBtn.classList.toggle('bi-list');
+    mobileNavToggleBtn.classList.toggle('bi-x');
+  }
+  mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navmenu a').forEach(navmenu => {
+    navmenu.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle('active');
+      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+      e.stopImmediatePropagation();
+    });
+  });
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Scroll top button
+   */
+  let scrollTop = document.querySelector('.scroll-top');
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
+
+//   RECIPE NEST SPECIFIC FUNCTIONS
+
+  /* Toggle/Hide badges */
+function loadBadges() {
+    document.querySelectorAll('.badge-bar').forEach((bar) => {
+        const badges = bar.querySelectorAll('.badge');
+        const moreBtn = bar.querySelector('.more-btn');
+
+        let remainingWidth = bar.offsetWidth - (moreBtn.offsetWidth + 5);
+
+        moreBtn.style.display = 'none';
+        badges.forEach(badge => {
+            badge.style.display = 'inline-block';
+        });
+
+        for (let i = 0; i < badges.length; i++) {
+            const badgeWidth = badges[i].offsetWidth + 5;
+
+            if (badgeWidth <= remainingWidth) {
+                remainingWidth -= badgeWidth;
+            } else {
+                let moreCount = badges.length - i;
+
+                for (let j = i; j < badges.length; j++) {
+                    badges[j].style.display = 'none';
+                }
+                moreBtn.textContent = `+${moreCount}`;
+                moreBtn.style.display = 'inline-block';
+                break;
+            }
+        }
+    });
+}
+
+    document.addEventListener("DOMContentLoaded",loadBadges);
+    window.addEventListener("load", loadBadges);
+    window.addEventListener("resize", loadBadges);
+
+const accountMenuToggleBtn = document.querySelector('.account-menu-toggle');
+function toggleAccountMenu() {
+    const menuOpened = "bi bi-chevron-down";
+    const menuClosed = "bi bi-chevron-up";
+
+    if (accountMenuToggleBtn.classList.contains(menuClosed)) {
+            accountMenuToggleBtn.classList.replace(menuClosed, menuOpened)
+    } else {
+            accountMenuToggleBtn.classList.replace(menuOpened, menuClosed)
+    };
+}
+
+    accountMenuToggleBtn.addEventListener('click', toggleAccountMenu)
+
+})();
--- resources/views/components/mylayouts/layout-prototype.blade.php
+++ resources/views/components/mylayouts/layout-prototype.blade.php
@@ -2,30 +2,17 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>RecipeNest</title>
-  <meta name="description" content="">
-  <meta name="keywords" content="">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>RecipeNest</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
 
-  <!-- Favicons -->
-  <link href="{{ asset('template_default/assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('template_default/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
-
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com" rel="preconnect">
-  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Poppins:wght@400;600;700;800;900&display=swap" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('template_default/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('template_default/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('template_default/assets/vendor/aos/aos.css') }}" rel="stylesheet">
-  <link href="{{ asset('template_default/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-
-  <!-- Main CSS File -->
-  <link href="{{ asset('template_default/assets/css/main.css') }}" rel="stylesheet">
-
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Poppins:wght@400;600;700;800;900&display=swap" rel="stylesheet">
+    @vite(['resources/js/app.js', 'resources/sass/app.scss'])
 </head>
 
 <body class="index-page">
@@ -34,115 +21,33 @@
 
 
     <div class="container position-relative d-flex align-items-center justify-content-between">
-
-      <a href="index.html" class="logo d-flex align-items-center me-auto me-xl-0">
-        <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1 class="sitename">RECIPEnest.</h1>
-      </a>
-
+        <a href="#" class="logo"><h1 class="sitename" style="text-decoration: none;">RECIPEnest</h1></a>
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="index.html" class="active">Home</a></li>
-          <li><a href="single-post.html">Recipes</a></li>
-          <li><a href="category.html">Chefs</a></li>
-          <li><a href="contact.html">Initiative</a></li>
-          <li><a href="" class="account-icon"><i class="bi bi-person-circle"></i><i class="bi bi-chevron-down toggle-dropdown"></i></a></li>
-          {{-- @auth
-                    <li class="dropdown">
-                        <a href="#" class="account-icon">
-                            <i class="bi bi-person-circle"></i>
-                            <i class="bi bi-chevron-down toggle-dropdown"></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="{{ route('profile') }}">My Profile</a></li>
-                            <li><a href="{{ route('filament.author.resources.recipes.index') }}">Dashboard</a></li>
-                            <li>
-                                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
-                            </li>
-                        </ul>
-                    </li>
-                    @else
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
-                    @endauth --}}
+            <li><a href="{{ route('recipes.index') }}">Recipes</a></li>
+            <li><a href="{{ route('chefs.index') }}">Chefs</a></li>
+
+            @guest
+            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a class="register-btn" href="{{ route('register') }}" >Sign In</a></li>
+            @endguest
+
+            @auth
+            <li><a>Dashboard</a></li>
+            <li>
+                <a href="{{ route('logout') }}"onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">Logout</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+            </li>
+            @endauth
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+
       </nav>
     </div>
   </header>
 
   <main class="main">
-
-    {{-- <!-- Slider Section -->
-    <section id="slider" class="slider section dark-background">
-
-      <div class="container" data-aos="fade-up" data-aos-delay="100">
-
-        <div class="swiper init-swiper">
-
-          <script type="application/json" class="swiper-config">
-            {
-              "loop": true,
-              "speed": 600,
-              "autoplay": {
-                "delay": 5000
-              },
-              "slidesPerView": "auto",
-              "centeredSlides": true,
-              "pagination": {
-                "el": ".swiper-pagination",
-                "type": "bullets",
-                "clickable": true
-              },
-              "navigation": {
-                "nextEl": ".swiper-button-next",
-                "prevEl": ".swiper-button-prev"
-              }
-            }
-          </script>
-
-          <div class="swiper-wrapper">
-
-            <div class="swiper-slide" style="background-image: url('assets/img/post-slide-1.jpg');">
-              <div class="content">
-                <h2><a href="single-post.html">The Best Homemade Masks for Face (keep the Pimples Away)</a></h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem neque est mollitia! Beatae minima assumenda repellat harum vero, officiis ipsam magnam obcaecati cumque maxime inventore repudiandae quidem necessitatibus rem atque.</p>
-              </div>
-            </div>
-
-            <div class="swiper-slide" style="background-image: url('assets/img/post-slide-2.jpg');">
-              <div class="content">
-                <h2><a href="single-post.html">17 Pictures of Medium Length Hair in Layers That Will Inspire Your New Haircut</a></h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem neque est mollitia! Beatae minima assumenda repellat harum vero, officiis ipsam magnam obcaecati cumque maxime inventore repudiandae quidem necessitatibus rem atque.</p>
-              </div>
-            </div>
-
-            <div class="swiper-slide" style="background-image: url('assets/img/post-slide-3.jpg');">
-              <div class="content">
-                <h2><a href="single-post.html">13 Amazing Poems from Shel Silverstein with Valuable Life Lessons</a></h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem neque est mollitia! Beatae minima assumenda repellat harum vero, officiis ipsam magnam obcaecati cumque maxime inventore repudiandae quidem necessitatibus rem atque.</p>
-              </div>
-            </div>
-
-            <div class="swiper-slide" style="background-image: url('assets/img/post-slide-4.jpg');">
-              <div class="content">
-                <h2><a href="single-post.html">9 Half-up/half-down Hairstyles for Long and Medium Hair</a></h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem neque est mollitia! Beatae minima assumenda repellat harum vero, officiis ipsam magnam obcaecati cumque maxime inventore repudiandae quidem necessitatibus rem atque.</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="swiper-button-next"></div>
-          <div class="swiper-button-prev"></div>
-
-          <div class="swiper-pagination"></div>
-        </div>
-
-      </div>
-
-    </section><!-- /Slider Section --> --}}
 
     {{ $slot }}
 
@@ -208,17 +113,7 @@
         <i class="bi bi-arrow-up-short"></i>
     </a>
 
-    <!-- Preloader -->
     <div id="preloader"></div>
-
-    <!-- Vendor JS Files -->
-    <script src="{{ asset('template_default/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('template_default/assets/vendor/php-email-form/validate.js') }}"></script>
-    <script src="{{ asset('template_default/assets/vendor/aos/aos.js') }}"></script>
-    <script src="{{ asset('template_default/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-
-    <!-- Main JS File -->
-    <script src="{{ asset('template_default/assets/js/main.js') }}"></script>
 
 </body>
 
